58d007d37031efda583feabaea848402220bc39d
Jose Manuel Cordoba Castillo <jose.m.cordoba.c@gmail.com>
2024-12-02 23:02:32 +0000

    Session1

---
+++ src/main/java/com/jose/cordoba/formacion/unitTesting/session1/dto/SumRequest.java
@@ -0,0 +1,4 @@
+package com.jose.cordoba.formacion.unitTesting.session1.dto;
+
+public record SumRequest(int a, int b) {
+}
---
+++ src/main/java/com/jose/cordoba/formacion/unitTesting/session1/service/OperationsService.java
@@ -0,0 +1,11 @@
+package com.jose.cordoba.formacion.unitTesting.session1.service;
+
+public interface OperationsService {
+
+    boolean alwaysTrue();
+    boolean alwaysFalse();
+
+    int sum(int a, int b);
+
+    int div(int num, int den);
+}
